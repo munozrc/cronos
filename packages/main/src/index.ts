2e49a60459f5fab9da98704e621fec98265dbd66
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-03 18:40:36 -0500

    refactor(Root): styling code

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,17 +1,17 @@
-import { app, BrowserWindow } from 'electron'
 import { join } from 'node:path'
+
+import { app, BrowserWindow } from 'electron'
 
 app.whenReady().then(main)
 
 // run when electron is ready!
 function main () {
-
   // Create a window and load a html file to be rendered.
   const window = new BrowserWindow({
-    width: 800, height: 700,
+    width: 800, height: 700
     // ...other window settings go here!
   })
 
   // Load the react app inside a chromium window.
-  window.loadFile(join(__dirname, "../../renderer/dist/index.html"))
+  window.loadFile(join(__dirname, '../../renderer/dist/index.html'))
 }
