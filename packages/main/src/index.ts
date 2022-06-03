9b95e362f9b849f9b188517b55e88d58c799142e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-03 18:15:30 -0500

    chore(Main): Create loader with electron

---
+++ packages/main/src/index.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'node:path'
+
+app.whenReady().then(main)
+
+// run when electron is ready!
+function main () {
+
+  // Create a window and load a html file to be rendered.
+  const window = new BrowserWindow({
+    width: 800, height: 700,
+    // ...other window settings go here!
+  })
+
+  // Load the react app inside a chromium window.
+  window.loadFile(join(__dirname, "../../renderer/dist/index.html"))
+}
