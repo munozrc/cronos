7789e802a641582a1426cac9c2c29bf4c9114e1c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-04 17:00:44 -0500

    feat(Renderer): Add new item and styles css for SearhView

--- packages/renderer/src/views/SearchView/index.tsx
+++ packages/renderer/src/views/SearchView/index.tsx
@@ -1,7 +1,27 @@
+import styles from './styles.module.css'
+
+const songData = {
+  title: 'Out of Time',
+  artist: 'The Weeknd',
+  album: 'Dawn FM',
+  albumCover: 'https://lh3.googleusercontent.com/5teqUPmWiFmagN0RggBKRXSW1zUj5_fVCEhbVhN6qt519EyHj6njy1x8dnJcRWNhQ5cl4dZgGaxbyqgv=w280-h280-l90-rj'
+}
+
 export const SearchView = () => {
   return (
-    <section>
-      <h3>Resultados</h3>
+    <section className={styles.wrapper}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <img className={styles.albumCover} src={songData.albumCover} />
+          <div className={styles.info}>
+            <h4 className={styles.titleSong}>{songData.title}</h4>
+            <div className={styles.subInfo}>
+              <p className={styles.artistSong}>{songData.artist}</p>
+              <p className={styles.albumSong}>{songData.album}</p>
+            </div>
+          </div>
+        </li>
+      </ul>
     </section>
   )
 }
