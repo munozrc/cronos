5a0d4680c433c7ba8f9d09f090775bbfe884b228
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-03 22:18:11 -0500

    chore(Root): Add config dev mode

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -2,16 +2,45 @@ import { join } from 'node:path'
 
 import { app, BrowserWindow } from 'electron'
 
-app.whenReady().then(main)
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
 
-// run when electron is ready!
-function main () {
+let window: BrowserWindow | null = null
+
+async function createMainWindow () {
   // Create a window and load a html file to be rendered.
-  const window = new BrowserWindow({
-    width: 800, height: 700
-    // ...other window settings go here!
+  window = new BrowserWindow({
+    title: 'Cronos - Music Downloader',
+    width: 900,
+    height: 600
   })
 
   // Load the react app inside a chromium window.
-  window.loadFile(join(__dirname, '../../renderer/dist/index.html'))
+  app.isPackaged
+    ? window.loadFile(join(__dirname, '../../renderer/dist/index.html'))
+    : window.loadURL('http://localhost:3000/')
 }
+
+// run when electron is ready!
+app.whenReady().then(createMainWindow)
+
+app.on('window-all-closed', () => {
+  window = null
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+  if (!window) return
+  // Focus on the main window if the user tried to open another
+  if (window.isMinimized()) window.restore()
+  window.focus()
+})
+
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows()
+  allWindows.length
+    ? allWindows[0].focus()
+    : createMainWindow()
+})
