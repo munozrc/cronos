e7ae4a44fdd90a0045a88fd0f3586ecdf89046b1
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-05 18:01:14 -0500

    feat(Main): Add service to search songs

--- packages/main/src/services.ts
+++ packages/main/src/services.ts
@@ -1,8 +1,18 @@
 import { searchMusics } from 'node-youtube-music'
 
 export async function searchSong (query: string) {
-  // TODO: Add validation for query
-  const musics = await searchMusics('As it was')
-  // TODO: return data
-  console.log({ musics })
+  if (query.trim() === '') return []
+  return await searchMusics(query)
+    .then(response => response.map(song => ({
+      id: song.youtubeId,
+      title: song.title,
+      artists: song.artists?.map(a => a.name).join(' - '),
+      duration: song.duration?.label,
+      album: song.album,
+      albumCover: song.thumbnailUrl
+    })))
+    .catch((err) => {
+      console.error('Error fetch data search: ', err)
+      return []
+    })
 }
