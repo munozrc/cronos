8fdbf8e1a0a9ef8933432f69fbbe04d4aae94ca2
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-06 17:13:16 -0500

    feat(Main): Create new method for download songs

--- packages/main/src/services.ts
+++ packages/main/src/services.ts
@@ -1,6 +1,13 @@
-import { searchMusics } from 'node-youtube-music'
+import { join } from 'node:path'
 
-import { Song } from '../../../typings'
+import { searchMusics } from 'node-youtube-music'
+import axios from 'axios'
+import ffmpeg from 'fluent-ffmpeg'
+import ytdl from 'ytdl-core'
+import id3 from 'node-id3'
+
+import { Song, Metadata } from '../../../typings'
+const pathDownloads = join(__dirname, '../../../../downloads')
 
 export function searchSong (query: string): Promise<Array<Song>> {
   return searchMusics(query)
@@ -16,4 +23,38 @@ export function searchSong (query: string): Promise<Array<Song>> {
       console.error('Error fetch data search: ', err)
       return []
     })
+}
+
+export async function downloadSong (metadata: Metadata): Promise<void> {
+  try {
+    const { id, title, artists, album, albumCover } = metadata
+    const stream = ytdl(id, { filter: 'audioonly' })
+    const filename = `${artists.join('& ')} - ${title}.mp3`
+    const pathFile = join(pathDownloads, filename)
+    const imageResponse = await axios.get(albumCover.toString(), { responseType: 'arraybuffer' })
+
+    const image = {
+      mime: 'image/png',
+      type: { id: 3, name: 'front cover' },
+      description: 'Album Art',
+      imageBuffer: imageResponse.data
+    }
+
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .format('mp3')
+      .save(pathFile)
+      .on('progress', (progress) => {
+        console.log(`${progress.targetSize}kb downloaded`)
+      })
+      .on('end', () => {
+        const artist = artists.join('; ')
+        id3.write({ title, artist, image, album }, pathFile)
+      })
+      .on('error', (err) => {
+        console.error('format buffer ffmpeg-error: ', err)
+      })
+  } catch (err) {
+    console.error('format buffer error: ', err)
+  }
 }
