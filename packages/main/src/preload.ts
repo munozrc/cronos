ea078924036798f1286e0292ce8d18e5ac2ade46
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-06 17:11:59 -0500

    feat(Main): Add new methods for preload

--- packages/main/src/preload.ts
+++ packages/main/src/preload.ts
@@ -1,10 +1,11 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
-import { searchSong } from './services'
+import { Metadata, Song } from '../../../typings'
 
 export const cronos = {
   closeWindow: () => ipcRenderer.send('closeWindow'),
-  searchSong
+  downloadSong: (metadata: Metadata): Promise<void> => ipcRenderer.invoke('downloadSong', metadata),
+  searchSong: (query: string): Promise<Array<Song>> => ipcRenderer.invoke('searchSong', query)
 }
 
 contextBridge.exposeInMainWorld('cronos', cronos)
