c49a0a28ac31e9af253069925917d89441ad0e38
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-07 12:52:21 -0500

    feat(gui): add styles for toolbar

--- gui/layouts/Toolbar.tsx
+++ gui/layouts/Toolbar.tsx
@@ -5,6 +5,11 @@ import styles from './Toolbar.module.css'
 export const Toolbar = () => {
   return (
     <header className={styles.header}>
+      <div className={styles.buttonGruop}>
+        <button className={styles.buttonClose} onClick={window.cronos.closeWindow}/>
+        <button className={styles.buttonMinimize} onClick={window.cronos.minimizeWindow}/>
+        <button className={styles.buttonMaximize} />
+      </div>
       <div className={styles.brand}>
         <h1 className={styles.title}>CRONOS</h1>
       </div>
