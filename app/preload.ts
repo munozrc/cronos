814029555942e7647d47051ebb2daef9c437cea3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-07 12:54:26 -0500

    fix(app): Create minimizeWindow

--- app/preload.ts
+++ app/preload.ts
@@ -4,6 +4,7 @@ import { Metadata, Song } from './types'
 
 export const cronos = {
   closeWindow: () => ipcRenderer.send('closeWindow'),
+  minimizeWindow: () => ipcRenderer.send('minimizeWindow'),
   downloadSong: (metadata: Metadata): Promise<void> => ipcRenderer.invoke('downloadSong', metadata),
   searchSong: (query: string): Promise<Array<Song>> => ipcRenderer.invoke('searchSong', query)
 }
