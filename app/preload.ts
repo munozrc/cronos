5fb38565f554688694204d2f3b3bd013273a8d23
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-09 23:24:35 -0500

    feat(app): Add function to open downloads folder to window.cronos

--- app/preload.ts
+++ app/preload.ts
@@ -5,7 +5,8 @@ export const cronos = {
   closeWindow: () => ipcRenderer.send('closeWindow'),
   minimizeWindow: () => ipcRenderer.send('minimizeWindow'),
   searchTrack: (query: string): Promise<Track[]> => ipcRenderer.invoke('searchTrack', query),
-  downloadTrack: (track: Track): Promise<void> => ipcRenderer.invoke('downloadTrack', track)
+  downloadTrack: (track: Track): Promise<void> => ipcRenderer.invoke('downloadTrack', track),
+  openDownloadsFolder: (): Promise<string> => ipcRenderer.invoke('openDownloadsFolder')
 }
 
 contextBridge.exposeInMainWorld('cronos', cronos)
