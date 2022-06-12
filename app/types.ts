6c71fe0edc4a9db4cd31418df2b2472313420989
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-11 19:53:26 -0500

    feat(app): Create new types for get Metada tracks

--- app/types.ts
+++ app/types.ts
@@ -12,3 +12,34 @@ export type TrackFile = Track & {
   state: 'completed' | 'downloading' | 'error'
   date?: { start: Date, end: Date }
 }
+
+export type iTunesMetadata = {
+  wrapperType: string
+  kind: string
+  artistId: number
+  collectionId: number
+  trackId: number
+  artistName: string
+  collectionName: string
+  trackName: string
+  releaseDate: string
+  trackCount: number
+  trackNumber: number
+  country: string
+  primaryGenreName: string
+}
+
+export type iTunesResponse = {
+  resultCount: number
+  results: Array<iTunesMetadata>
+}
+
+export type TagImage = {
+  mime: string
+  type: {
+    id: number
+    name: string
+  }
+  description: string
+  imageBuffer: Buffer
+}
