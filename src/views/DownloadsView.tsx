ee1a2bf027f9ecbf127b83f00f85cefcf01e7f1a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-16 12:25:43 -0500

    fix(app): Use uuid as key prop DownloadView

--- src/views/DownloadsView.tsx
+++ src/views/DownloadsView.tsx
@@ -22,8 +22,8 @@ export const DownloadsView = () => {
           <th>Estado</th>
           <th>Fecha</th>
         </tr>
-        {itemList.map(({ title, artists, state, date, id }) => (
-          <tr key={date.getMilliseconds()}>
+        {itemList.map(({ title, artists, state, date, uuid }) => (
+          <tr key={uuid}>
             <td>{`${artists.join(' & ')} - ${title}`}</td>
             <td className={`${styles.status} ${styles['status-' + state]}`}>{stateTransform[state]}</td>
             <td>{dateFormat(date)}</td>
