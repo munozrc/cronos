af29f59d73b2406aba40ab9d7414e8b634724157
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-16 21:57:43 -0500

    feat(app): Implement searchTrackSuggestions

--- src/stores/useAppStore.tsx
+++ src/stores/useAppStore.tsx
@@ -1,22 +1,38 @@
 import create from 'zustand'
 import { Track } from '../../electron/types'
 
-type QueryStatus = 'loading' | 'error' | 'complete'
+type ResponseStatus = 'loading' | 'error' | 'complete'
 
 interface AppState {
   lastQuery: string
   queryResults: Array<Track>
-  queryStatus: QueryStatus
+  suggestionResults: Array<Track>
+  queryStatus: ResponseStatus
+  suggestionStatus: ResponseStatus
   setNewQuery: (query: string) => void
   setQueryResults: (results: Array<Track>) => void
-  setStatusQuery: (status: QueryStatus) => void
+  setStatusQuery: (status: ResponseStatus) => void
+  searchTrackSuggestions: () => void
 }
 
-export const useAppStore = create<AppState>((set) => ({
+export const useAppStore = create<AppState>((set, get) => ({
   lastQuery: '',
   queryResults: [],
+  suggestionResults: [],
   queryStatus: 'complete',
+  suggestionStatus: 'complete',
   setNewQuery: (query: string) => { set(() => ({ lastQuery: query })) },
   setQueryResults: (results: Array<Track>) => { set(() => ({ queryResults: results })) },
-  setStatusQuery: (status: QueryStatus) => { set(() => ({ queryStatus: status })) }
+  setStatusQuery: (status: ResponseStatus) => { set(() => ({ queryStatus: status })) },
+  searchTrackSuggestions: () => {
+    const { getTrackSuggestions } = window.cronos
+    const { queryResults, queryStatus } = get()
+
+    if (queryResults.length === 0 || queryStatus === 'error') return
+
+    set(() => ({ suggestionStatus: 'loading' }))
+    getTrackSuggestions(queryResults[0].id)
+      .then(suggestions => set(() => ({ suggestionResults: suggestions, suggestionStatus: 'complete' })))
+      .catch(() => set(() => ({ suggestionResults: [], suggestionStatus: 'error' })))
+  }
 }))
