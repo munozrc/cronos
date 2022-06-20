edb14657395772a390aae8b7f13fc5a94c2b75db
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-19 20:15:13 -0500

    fix(app): Add padding to title Hero

--- src/layouts/Hero/index.tsx
+++ src/layouts/Hero/index.tsx
@@ -4,15 +4,16 @@ import styles from './styles.module.css'
 
 interface HeroProps {
   icon: ReactNode
-  subtitle?: string
+  title: string
+  subtitle: string
 }
 
-export const Hero = ({ icon, subtitle }: HeroProps) => {
+export const Hero = ({ icon, title, subtitle }: HeroProps) => {
   return (
     <header className={styles.wrapperHeader}>
       <span className={styles.wrapperIcon}>{icon}</span>
       <section>
-        <h3 className={styles.title}>Descargas</h3>
+        <h3 className={styles.title}>{title}</h3>
         <p className={styles.subtitle}>{subtitle}</p>
       </section>
     </header>
