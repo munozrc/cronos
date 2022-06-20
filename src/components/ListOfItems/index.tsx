7c072184e530cd3c58f9c7fc0f796b908e7b63dc
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-19 19:16:47 -0500

    fix(app): Add new props for ListOfItems

--- src/components/ListOfItems/index.tsx
+++ src/components/ListOfItems/index.tsx
@@ -1,48 +1,41 @@
 import { ReactNode } from 'react'
-import { useDownloadStore } from '../../stores/useDownloadStore'
 import { Track } from '../../../electron/types'
 import { Button } from '../Button'
 
 import styles from './styles.module.css'
 
-interface ListOfItemsProps { children: ReactNode }
-interface ResultItemProps extends Track {}
-
-const ListOfItems = ({ children }: ListOfItemsProps) => {
-  return (
-    <ul className={styles.wrapperList}>
-      {children}
-    </ul>
-  )
+interface ListOfItemsProps {
+  children: ReactNode
+}
+interface ResultItemProps extends Track {
+  handleDownload: () => void
+  handlePlaySong: () => void
 }
 
-const ResultItem = (props: ResultItemProps) => {
-  const { albumCover, artist, title, album } = props
-  const { createNewDownload } = useDownloadStore()
+const ListOfItems = ({ children }: ListOfItemsProps) => (
+  <ul className={styles.wrapperList}>
+    {children}
+  </ul>
+)
 
-  // Handling functions
-  const handleDownload = () => createNewDownload(props)
-  const handlePlaySong = () => {}
-
-  return (
-    <li className={styles.wrapperItem}>
-      <picture className={styles.albumCover}>
-        <img src={albumCover} loading="lazy" alt={artist}/>
-      </picture>
-      <div className={styles.wrapperInfo}>
-        <h4 className={styles.titleSong}>{title}</h4>
-        <div className={styles.wrapperSubtitle}>
-          <p className={styles.artistSong}>{artist}</p>
-          <p className={styles.albumSong}>{album}</p>
-        </div>
+const ResultItem = ({ albumCover, artist, title, album, ...props }: ResultItemProps) => (
+  <li className={styles.wrapperItem}>
+    <picture className={styles.albumCover}>
+      <img src={albumCover} loading="lazy" alt={artist}/>
+    </picture>
+    <div className={styles.wrapperInfo}>
+      <h4 className={styles.titleSong}>{title}</h4>
+      <div className={styles.wrapperSubtitle}>
+        <p className={styles.artistSong}>{artist}</p>
+        <p className={styles.albumSong}>{album}</p>
       </div>
-      <div className={styles.wrapperOptions}>
-        <Button variant='outline' onClick={handlePlaySong}>Reproducir</Button>
-        <Button variant='outline' onClick={handleDownload}>Descargar</Button>
-      </div>
-    </li>
-  )
-}
+    </div>
+    <div className={styles.wrapperOptions}>
+      <Button variant='outline' onClick={props.handlePlaySong}>Reproducir</Button>
+      <Button variant='outline' onClick={props.handleDownload}>Descargar</Button>
+    </div>
+  </li>
+)
 
 export { ResultItem }
 export default ListOfItems
