81abcdcaf1be79b855d0acea28a052294647bd4f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-20 19:45:41 -0500

    feat(app): use new TabContainer

--- src/views/SearchView/index.tsx
+++ src/views/SearchView/index.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import { useDownloadStore } from '../../stores/useDownloadStore'
 import { useAppStore } from '../../stores/useAppStore'
-import { ViewContainer } from '../../layouts'
 import { ListOfItems } from '../../components/ListOfItems'
 import { Spinner } from '../../components/Spinner'
+import { TabContainer } from '../../layouts/TabContainer'
 
 export const SearchView = () => {
-  const [showSuggestions, toggleSuggestions] = useState<boolean>(false)
   const { queryResults, queryStatus, suggestionResults } = useAppStore()
   const { createNewDownload } = useDownloadStore()
 
@@ -14,21 +12,15 @@ export const SearchView = () => {
   if (queryStatus === 'error') return <h3>Algo salio mal!</h3>
 
   return (
-    <ViewContainer>
-      <div>
-        <button onClick={() => toggleSuggestions(false)}>resultados</button>
-        <button onClick={() => toggleSuggestions(true)}>sugerencias</button>
-      </div>
+    <TabContainer>
       <ListOfItems
         list={queryResults}
-        isVisible={!showSuggestions}
         createNewDownload={createNewDownload}
       />
       <ListOfItems
         list={suggestionResults}
-        isVisible={showSuggestions}
         createNewDownload={createNewDownload}
       />
-    </ViewContainer>
+    </TabContainer>
   )
 }
