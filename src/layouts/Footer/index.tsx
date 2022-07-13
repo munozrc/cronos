28fd5dccb7e34e57f5c8246c461f3522a9d34275
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-13 16:22:33 -0500

    feat(Root): new footer component

---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+export function Footer () {
+  return (
+    <footer className={styles.container}>
+      <div>
+        <p>by <strong>@munozrc</strong></p>
+      </div>
+    </footer>
+  )
+}
