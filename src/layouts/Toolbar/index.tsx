7e557b190b00b09bb38792921729d3b5a8149742
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-15 22:04:32 -0500

    feat(App): New styles for Toolbar

--- src/layouts/Toolbar/index.tsx
+++ src/layouts/Toolbar/index.tsx
@@ -1,31 +1,15 @@
-import { Link } from 'react-router-dom'
-import { DownloadIcon, SearchIcon, SettingsIcon } from '../../components/Icons'
-
 import styles from './styles.module.css'
 
 export const Toolbar = () => {
   const { closeWindow, minimizeWindow } = window.cronos
   return (
-    <header className={styles.wrapper}>
-      <div className={styles.branding}>
-        <h1 className={styles.title}>CRONOS</h1>
-        <p className={styles.subtitle}>Music Downloader</p>
-      </div>
-      <nav className={styles.actionsLinks}>
-        <Link to={'/'} className={styles.link}>
-          <SearchIcon />
-        </Link>
-        <Link to={'downloads'} className={styles.link}>
-          <DownloadIcon />
-        </Link>
-        <Link to={'settings'} className={styles.link}>
-          <SettingsIcon />
-        </Link>
-      </nav>
-      <div className={styles.actionsButtons}>
-        <button className={styles.buttonMaximize} />
-        <button className={styles.buttonMinimize} onClick={minimizeWindow}/>
-        <button className={styles.buttonClose} onClick={closeWindow}/>
+    <header className={styles.container}>
+      <h1 className={styles.logo}>CRONOS</h1>
+      <p className={styles.subtitle}>Music Downloader</p>
+      <div className={styles.buttons}>
+        <button className={styles.btnMaximize} />
+        <button className={styles.btnMinimize} onClick={minimizeWindow}/>
+        <button className={styles.btnClose} onClick={closeWindow}/>
       </div>
     </header>
   )
