b9b4da45a5ca5446c6f4e8dc517f37470b3a1107
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-15 22:05:07 -0500

    feat(App): Create sidebar component

---
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import { DownloadIcon, FolderIcon, SearchIcon, SettingsIcon } from '../../components/Icons'
+
+import styles from './Sidebar.module.css'
+
+export const Sidebar = () => (
+  <nav className={styles.container}>
+    <Link to="/" className={styles.itemLink}>
+      <SearchIcon height="23px" width="23px" />
+    </Link>
+    <Link to="/downloads" className={styles.itemLink}>
+      <DownloadIcon height="24px" width="24px" />
+    </Link>
+    <Link to="/" className={styles.itemLink}>
+      <FolderIcon height="24px" width="24px" />
+    </Link>
+    <Link to="/settings" className={styles.itemLink}>
+      <SettingsIcon height="24px" width="24px" />
+    </Link>
+  </nav>
+)
