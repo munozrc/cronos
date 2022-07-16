d1236bac12039e950e005fc9a353b8efcdd8c61d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-15 22:12:22 -0500

    fix(App): Remove all content to SearchView

--- src/views/SearchView/index.tsx
+++ src/views/SearchView/index.tsx
@@ -1,34 +1,8 @@
-import { useDownloadStore } from '../../stores/useDownloadStore'
-import { useAppStore } from '../../stores/useAppStore'
-import { ListOfItems } from '../../components/ListOfItems'
-import { Spinner } from '../../components/Spinner'
-import { Searchbar, TabContainer, ViewContainer } from '../../layouts'
+import { ViewContainer } from '../../layouts'
 
 export const SearchView = () => {
-  const { queryResults, queryStatus, suggestionResults, searchTrackSuggestions } = useAppStore()
-  const { createNewDownload } = useDownloadStore()
-
-  const handleCallbackTab = (index: number) => {
-    if (index === 1) searchTrackSuggestions()
-  }
-
   return (
     <ViewContainer>
-      <Searchbar />
-      {queryStatus === 'complete' && (
-        <TabContainer callback={handleCallbackTab}>
-          <ListOfItems
-            list={queryResults}
-            createNewDownload={createNewDownload}
-          />
-          <ListOfItems
-            list={suggestionResults}
-            createNewDownload={createNewDownload}
-          />
-        </TabContainer>
-      )}
-      {queryStatus === 'loading' && <Spinner />}
-      {queryStatus === 'error' && <h3>Algo salio mal!</h3>}
     </ViewContainer>
   )
 }
