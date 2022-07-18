0fa4b1ff2ce0f8349ce963dd154ed4bcb7449028
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-18 16:37:05 -0500

    refactor(App): Remove spinner component

--- src/components/Spinner/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css'
-
-export const Spinner = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.element} />
-    </div>
-  )
-}
