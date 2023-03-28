245f3d6645c3a919dd68a5e7efde631385211ae3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-03-28 15:42:51 -0500

    refactor(renderer): Change name ToastComponent

--- src/renderer/components/Toast/ToastComponent.tsx
+++ src/renderer/components/Toast/ToastComponent.tsx
@@ -1,5 +1,10 @@
-import { type ReactNode } from "react"
+import type { ToastContent, ToastState } from "@/global"
 import styles from "./Toast.module.css"
+
+interface ToastProps {
+  state: ToastState
+  content: ToastContent
+}
 
 const stateTitle = {
   pending: "Pendiente",
@@ -7,14 +12,9 @@ const stateTitle = {
   error: "Error"
 }
 
-interface ToastProps {
-  state: "pending" | "success" | "error"
-  children: ReactNode
-}
-
-export const Toast: React.FC<ToastProps> = ({ state, children }) => (
+export const ToastComponent: React.FC<ToastProps> = ({ state, content }) => (
   <div className={styles.container}>
     <h4 className={`${styles.title} ${styles[state]}`}>{stateTitle[state]}</h4>
-    <p className={styles.content}>{children}</p>
+    <p className={styles.content}>{content}</p>
   </div>
 )
--- src/renderer/components/Toast/ToastContainer.tsx
+++ src/renderer/components/Toast/ToastContainer.tsx
@@ -1,11 +1,38 @@
-import { Toast } from "./Toast"
+import { type Toast } from "@/global"
+import { useEffect, useState } from "react"
+import { ToastComponent } from "./ToastComponent"
 import styles from "./ToastContainer.module.css"
 
 export const ToastContainer: React.FC = () => {
+  const [toastList, setToastList] = useState<Toast[]>([])
+
+  useEffect(() => {
+    const createToast = (e: CustomEvent<string>): void => {
+      setToastList(prev => ([
+        ...prev,
+        {
+          id: window.crypto.randomUUID(),
+          content: e.detail,
+          state: "pending"
+        }
+      ]))
+    }
+
+    window.addEventListener("createtoast", createToast as EventListener)
+    return () => {
+      window.removeEventListener("createtoast", createToast as EventListener)
+    }
+  })
+
   return (
     <div className={styles.container}>
-      <Toast state="success">Last Christmas - Wham!</Toast>
-      <Toast state="pending">Last Christmas - Wham!</Toast>
+      {toastList.map(({ id, state, content }) => (
+        <ToastComponent
+          key={id}
+          state={state}
+          content={content}
+        />
+      ))}
     </div>
   )
 }
