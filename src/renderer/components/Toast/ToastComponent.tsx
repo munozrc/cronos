3e0f8076f62e4487ec497e5fe223920b42232e6b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-03-28 19:42:35 -0500

    feat(renderer): Change name styles css and set timer

--- src/renderer/components/Toast/ToastComponent.tsx
+++ src/renderer/components/Toast/ToastComponent.tsx
@@ -1,10 +1,8 @@
-import type { ToastContent, ToastState } from "@/types"
-import styles from "./Toast.module.css"
+import { useCallback, useEffect, useRef } from "react"
+import type { Toast } from "@/types"
+import { useToast } from "@/hooks"
 
-interface ToastProps {
-  state: ToastState
-  content: ToastContent
-}
+import styles from "./ToastComponent.module.css"
 
 const stateTitle = {
   pending: "Pendiente",
@@ -12,9 +10,33 @@ const stateTitle = {
   error: "Error"
 }
 
-export const ToastComponent: React.FC<ToastProps> = ({ state, content }) => (
-  <div className={styles.container}>
-    <h4 className={`${styles.title} ${styles[state]}`}>{stateTitle[state]}</h4>
-    <p className={styles.content}>{content}</p>
-  </div>
-)
+export const ToastComponent: React.FC<Toast> = ({ id, state, content }) => {
+  const refTimer = useRef<number>()
+  const { removeToast } = useToast()
+
+  const startTimer = useCallback((): void => {
+    if (refTimer.current !== null) return
+    refTimer.current = window.setTimeout(() => {
+      removeToast(id)
+    }, 5000)
+  }, [id, removeToast])
+
+  useEffect(() => {
+    if (state === "success") {
+      startTimer()
+      console.log("Timer Start ---> ", content)
+    }
+
+    return () => {
+      if (refTimer.current === null) return
+      window.clearTimeout(refTimer.current)
+    }
+  }, [state, content, startTimer])
+
+  return (
+    <div className={styles.container}>
+      <h4 className={`${styles.title} ${styles[state]}`}>{stateTitle[state]}</h4>
+      <p className={styles.content}>{content}</p>
+    </div>
+  )
+}
