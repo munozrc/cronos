f7cdd1aad4f7941d2343243b516eb75dc82d3c06
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-06-22 00:42:09 -0500

    chore(package/frontend): Initial file structure

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <main>
+      <header>
+        <h1>Cronos | Music Downloader</h1>
+      </header>
+    </main>
+  );
+};
