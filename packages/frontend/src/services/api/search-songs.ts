c4db62ec853e257787129ca9c3e9671abc7b8bc7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-08-03 23:52:15 -0500

    feat(frontend): Create service for search a song

---
+++ packages/frontend/src/services/api/search-songs.ts
@@ -0,0 +1,21 @@
+export async function searchSong(query: string) {
+  try {
+    const response = await fetch(`/api/search/${query}`);
+
+    if (!response.ok) {
+      throw new Error(`Error searching for the song: ${response.statusText}`);
+    }
+
+    const jsonResponse = await response.json();
+    const { results } = jsonResponse;
+
+    if (!Array.isArray(results)) {
+      throw new Error("Invalid response format: results is not an array");
+    }
+
+    return results;
+  } catch (error) {
+    console.error("Error searching for the song:", error);
+    throw error;
+  }
+}
