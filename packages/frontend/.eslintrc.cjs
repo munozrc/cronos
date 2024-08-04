0cd17cc557eca2689a937d0e1ddc12ee1d9050ff
Carlos Muñoz <munozrengifocarlos@gmail.com>
2024-08-03 23:51:48 -0500

    feat(frontend): Add react-query

--- packages/frontend/.eslintrc.cjs
+++ packages/frontend/.eslintrc.cjs
@@ -1,4 +1,7 @@
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
 module.exports = {
+  ...pluginQuery.configs["flat/recommended"],
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -13,9 +16,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
 };
